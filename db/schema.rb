e47a95c811aca3c089ff32946cc8e9cb8bb0c0aa
NiranjanKumar003 <niranjankumarofficial003@gmail.com>
2026-01-10 22:33:27 +0530

    pg

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,17 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.1].define(version: 2026_01_09_122528) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
+  create_table "cars", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "make"
+    t.string "model"
+    t.datetime "updated_at", null: false
+    t.integer "year"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email"
